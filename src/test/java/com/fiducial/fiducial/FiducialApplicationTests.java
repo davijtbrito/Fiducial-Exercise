536870a34f040f8eb37bdcc511d3999c9401bf32
davibrito1218 <davibrito1218@gmail.com>
2023-02-15 11:03:08 +0000

    init commit

---
+++ src/test/java/com/fiducial/fiducial/FiducialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiducial.fiducial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiducialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
