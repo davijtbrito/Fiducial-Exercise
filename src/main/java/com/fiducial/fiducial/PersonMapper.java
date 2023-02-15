c8e00046d0ff82e0e03c05b3fe081f631371737c
davibrito1218 <davibrito1218@gmail.com>
2023-02-15 14:40:31 +0000

    Adding javadoc

--- src/main/java/com/fiducial/fiducial/PersonMapper.java
+++ src/main/java/com/fiducial/fiducial/PersonMapper.java
@@ -1,15 +1,29 @@
 package com.fiducial.fiducial;
 
 /**
- * this mapper will be responsable for the entity and dto @Person.
+ * {@summary this mapper will be responsable for the entity and dto.}
+ * {@literal }
+ * {@literal see also: } 
+ * {@link PersonDto} and {@link Person}
  */
 public class PersonMapper {
     
+    /**
+     * Returns the an Entity for the PersonDto.
+     * @param dto {@link PersonDto}
+     * @return {@link Person}
+     */
     public static Person getEntity(PersonDto dto){
 
         return new Person(dto.getId(), dto.getName());
     }
 
+
+    /**
+     * Returns the a Dto for the Person.
+     * @param dto {@link Person}
+     * @return {@link PersonDto}
+     */
     public static PersonDto getDto(Person entity){
 
         return new PersonDto(entity.getId(), entity.getName());
--- src/main/java/com/fiducial/fiducial/PersonRepository.java
+++ src/main/java/com/fiducial/fiducial/PersonRepository.java
@@ -2,7 +2,15 @@ package com.fiducial.fiducial;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository for the entity {@link Person} 
+ */
 public interface PersonRepository extends JpaRepository<Person, Long>{    
 
+    /**
+     * Returns the Person using the name as key.
+     * @param name
+     * @return {@link Person}
+     */
     public Person findByName(String name);      
 }
